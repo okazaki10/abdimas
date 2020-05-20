3f40855da2717c8a90bec78917a51db485f9a2fa
rakha abadi susilo <rakaabadisusilo@gmail.com>
2020-05-20 16:33:47 +0700

    Revert "ngga jelas"
    This reverts commit 26d0c4736bb90ccc33baec4ef36c78399181ce23.

--- public/js/app.js
+++ public/js/app.js
@@ -20312,74 +20312,79 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c(
-    "nav",
-    {
-      staticClass: "flex items-center justify-between flex-wrap bg-teal-500 p-6"
-    },
-    [
-      _c(
-        "div",
-        { staticClass: "flex items-center flex-shrink-0 text-white mr-6" },
-        [
-          _c(
-            "svg",
-            {
-              staticClass: "fill-current h-8 w-8 mr-2",
-              attrs: {
-                width: "54",
-                height: "54",
-                viewBox: "0 0 54 54",
-                xmlns: "http://www.w3.org/2000/svg"
-              }
-            },
-            [
-              _c("path", {
-                attrs: {
-                  d:
-                    "M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"
-                }
-              })
-            ]
-          ),
-          _vm._v(" "),
-          _c("span", { staticClass: "font-semibold text-xl tracking-tight" }, [
-            _vm._v("Tailwind CSS")
-          ])
-        ]
-      ),
-      _vm._v(" "),
-      _c("div", { staticClass: "block lg:hidden" }, [
+  return _c("div", [
+    _c(
+      "nav",
+      {
+        staticClass:
+          "flex items-center justify-between flex-wrap bg-teal-500 p-6"
+      },
+      [
         _c(
-          "button",
-          {
-            staticClass:
-              "flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
-          },
+          "div",
+          { staticClass: "flex items-center flex-shrink-0 text-white mr-6" },
           [
             _c(
               "svg",
               {
-                staticClass: "fill-current h-3 w-3",
+                staticClass: "fill-current h-8 w-8 mr-2",
                 attrs: {
-                  viewBox: "0 0 20 20",
+                  width: "54",
+                  height: "54",
+                  viewBox: "0 0 54 54",
                   xmlns: "http://www.w3.org/2000/svg"
                 }
               },
               [
-                _c("title", [_vm._v("Menu")]),
                 _c("path", {
-                  attrs: { d: "M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" }
+                  attrs: {
+                    d:
+                      "M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"
+                  }
                 })
               ]
+            ),
+            _vm._v(" "),
+            _c(
+              "span",
+              { staticClass: "font-semibold text-xl tracking-tight" },
+              [_vm._v("Tailwind CSS")]
             )
           ]
-        )
-      ]),
-      _vm._v(" "),
-      _vm._m(0)
-    ]
-  )
+        ),
+        _vm._v(" "),
+        _c("div", { staticClass: "block lg:hidden" }, [
+          _c(
+            "button",
+            {
+              staticClass:
+                "flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+            },
+            [
+              _c(
+                "svg",
+                {
+                  staticClass: "fill-current h-3 w-3",
+                  attrs: {
+                    viewBox: "0 0 20 20",
+                    xmlns: "http://www.w3.org/2000/svg"
+                  }
+                },
+                [
+                  _c("title", [_vm._v("Menu")]),
+                  _c("path", {
+                    attrs: { d: "M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" }
+                  })
+                ]
+              )
+            ]
+          )
+        ]),
+        _vm._v(" "),
+        _vm._m(0)
+      ]
+    )
+  ])
 }
 var staticRenderFns = [
   function() {
@@ -20400,7 +20405,7 @@ var staticRenderFns = [
                 "block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4",
               attrs: { href: "#responsive-header" }
             },
-            [_vm._v("\n        Docs\n      ")]
+            [_vm._v("\r\n        Docs\r\n      ")]
           ),
           _vm._v(" "),
           _c(
@@ -20410,7 +20415,7 @@ var staticRenderFns = [
                 "block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4",
               attrs: { href: "#responsive-header" }
             },
-            [_vm._v("\n        Examples\n      ")]
+            [_vm._v("\r\n        Examples\r\n      ")]
           ),
           _vm._v(" "),
           _c(
@@ -20420,7 +20425,7 @@ var staticRenderFns = [
                 "block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white",
               attrs: { href: "#responsive-header" }
             },
-            [_vm._v("\n        Blog\n      ")]
+            [_vm._v("\r\n        Blog\r\n      ")]
           )
         ]),
         _vm._v(" "),
