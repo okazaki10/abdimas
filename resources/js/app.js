c4cabffffb29e6d6ea79bdc85883d18638060bbe
rakha abadi susilo <rakaabadisusilo@gmail.com>
2020-05-20 16:51:49 +0700

    dd
    a

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,8 +7,6 @@ import Articles from './components/Articles.vue';
 Vue.component('articles',Articles);
 import Form_rehabilitasi from './components/Form_rehabilitasi.vue';
 Vue.component('form_rehabilitasi',Form_rehabilitasi);
-import Navbar from './components/Navbar.vue';
-Vue.component('navbar',Navbar);
 const app = new Vue({
     el:'#app'
 });
