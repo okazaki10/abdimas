b7d0e6bddeaac0f5e481eac1308fa4cbbce6edbd
rakha abadi susilo <rakaabadisusilo@gmail.com>
2020-05-20 16:57:46 +0700

    wow xavier benar

--- resources/views/rehabilitasi/rehabilitasi.blade.php
+++ resources/views/rehabilitasi/rehabilitasi.blade.php
@@ -36,6 +36,7 @@
             <form class="bg-white px-8 pt-6 pb-8 mb-4" method="POST" enctype="multipart/form-data" action="{{action('RehabilitasiController@store')}}">
                 {{csrf_field()}}
         <form_rehabilitasi></form_rehabilitasi>
+            </form>
         <script src="{{asset('js/app.js')}}"></script>
     </div>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('/','RehabilitasiController');
+Route::resource('rehabilitasi','RehabilitasiController');
 Route::resource('homepage','HomePageController');
