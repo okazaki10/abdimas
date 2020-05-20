0c49d313d42981cd05dae9a9ef60228196aec88d
rakha abadi susilo <rakaabadisusilo@gmail.com>
2020-05-20 16:49:03 +0700

    s

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,13 +13,7 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     </head>
-<<<<<<< HEAD
-<<<<<<< HEAD
  
-=======
->>>>>>> parent of 4e6cb82... navbar
-=======
->>>>>>> parent of 4e6cb82... navbar
     <body>
         <div id="app">
          
