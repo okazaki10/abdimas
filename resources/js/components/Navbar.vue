573e18e22067742d99497c1d12bfc635c06af145
Xavier Wahyuadi <xavierwahyuadi2505@gmail.com>
2020-05-22 00:06:27 +0700

    Update Navbar.vue

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -12,8 +12,8 @@
           <path
             d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"
           />
-        </svg> -->
-        <img src="public\700px-City_of_Surabaya_Logo.svg.png">
+        </svg>-->
+        <img src="https://upload.wikimedia.org/wikipedia/commons/b/ba/City_of_Surabaya_Logo.svg" />
         <div class="h-64 grid grid-flow-col gap-1">
           <span class="font-bold text-xl tracking-tight">Pemerintah Kota Surabaya</span>
           <span class="font-semibold text-xl tracking-tight">RSUD Dr. Mohammad Soewandi</span>
